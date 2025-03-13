558d214a353bfe7ca700d1328f2e7459bfaba310
Matt <mattymeans@gmail.com>
2025-03-13 00:01:32 -0600

    updated tel link

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -13,7 +13,7 @@ function Contact(){
                 This is where your contact info goes. You could have a telephone number, email, and address. You could even have google maps integrated right here!
                 The email can even be set up so that when it is clicked it will open the relevant application to send an email. Give it a try <a href="mailto: matt_means.dev@outlook.com" className='text-blue-700'> matt_means.dev@outlook.com</a>
             </p>
-            <p className='text-[1.5rem] lg:text-[2rem]'>Same for the telephone number for mobile devices! <a href='tele: +15555555555' className='text-blue-700' >555-555-5555</a></p>  
+            <p className='text-[1.5rem] lg:text-[2rem]'>Same for the telephone number for mobile devices! <a href='tel: +15555555555' className='text-blue-700' >555-555-5555</a></p>  
             </div>
             
             <Footer
