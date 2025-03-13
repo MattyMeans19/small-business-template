ab20df61aa5884c4d3b5e96d264d03b315b044d8
Matt <mattymeans@gmail.com>
2025-03-13 00:10:01 -0600

    adjust footer

--- src/About.jsx
+++ src/About.jsx
@@ -8,7 +8,7 @@ function About(){
         <div className='relative min-h-[100vh]'>
             <Banner />
             <Navbar />
-            <div className="relative border-solid border-5 text-center mt-[10%] mx-[10%] p-5 bg-sky-300">
+            <div className="relative border-solid border-5 text-center my-[10%] mx-[10%] p-5 bg-sky-300">
                 <h1 className="text-[2rem]">Here you could display all the information you want about your business/services for potential customers to see.</h1>
                 <p className="text-[2rem]">It can be as long or short as you need it to be and have a variety of other things like photos or videos!</p>
             </div>
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,7 +8,7 @@ function Contact(){
         <div className='min-h-[100vh] relative'>
             <Banner />
             <Navbar />
-            <div className='text-center border-solid border-5 p-5 mx-[5%] lg:mx-[10%] my-[5%] text-[1.5em] bg-sky-300'>
+            <div className='text-center border-solid border-5 p-5 mx-[5%] lg:mx-[10%] my-[10%] text-[1.5em] bg-sky-300'>
               <p className='text-[1.5rem] lg:text-[2rem]'>
                 This is where your contact info goes. You could have a telephone number, email, and address. You could even have google maps integrated right here!
                 The email can even be set up so that when it is clicked it will open the relevant application to send an email. Give it a try <a href="mailto: matt_means.dev@outlook.com" className='text-blue-700'> matt_means.dev@outlook.com</a>
